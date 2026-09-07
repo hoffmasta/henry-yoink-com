8cd9e64e504b1fffbefae50c41e82aed8bc128bb
topicalfish <115113802+hoffmasta@users.noreply.github.com>
2026-09-07 16:57:04 -0500

    Add files via upload

---
+++ Scripts/version.js
@@ -0,0 +1,25 @@
+//Whenever changes to betterDict are made and the version is updated,
+//the below version must match exactly.
+
+version = "1.3";
+
+/*-----------------------------------
+
+  __
+ (_    _.  ._ _   __
+ __)  (_|  | | |
+
+
+    Thanks for everything man! You single-handledly have
+made me feel 𝘯𝘰𝘵 𝘢𝘭𝘰𝘯𝘦 in the realm of coding, and it
+has literally 𝘮𝘦𝘢𝘯𝘵 𝘵𝘩𝘦 𝘸𝘰𝘳𝘭𝘥. If you 𝘦𝘷𝘦𝘳 need something,
+have a question, idea, or just want to chat, feel 𝘧𝘳𝘦𝘦
+to reach out! I wish you the best for your next couple
+years at Valley— take care of Coach for me lol. Happy
+coding, and thanks for keeping this project updated
+for the school!
+
+      - 𝓗𝓮𝓷𝓻𝔂
+
+
+-----------------------------------*/
